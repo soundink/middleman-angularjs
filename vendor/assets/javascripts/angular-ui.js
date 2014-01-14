b04410f40ec8475855edfc05fe4fe489fb3cd7f5
lizhe <mark.geek.lee@gmail.com>
2014-01-14 15:22:11 +0800

    init with angular angular-ui jquery

---
+++ vendor/assets/javascripts/angular-ui.js
@@ -0,0 +1,2 @@
+//= require angular-ui/0.9/core
+//= require angular-ui/0.9/tpls
---
+++ vendor/assets/javascripts/angular.js
@@ -0,0 +1,7 @@
+//= require ./angular/1.2.4/core
+//= require ./angular/1.2.4/cookies
+//= require ./angular/1.2.4/animate
+//= require ./angular/1.2.4/mocks
+//= require ./angular/1.2.4/resource
+//= require ./angular/1.2.4/route
+//= require ./angular/1.2.4/sanitize
